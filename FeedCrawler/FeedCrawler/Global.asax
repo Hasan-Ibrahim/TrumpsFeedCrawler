1973d788dc83a9c88ed10243fd093118a44ab2bc
Shuvo <hasan.ibrahim.shuvo@gmail.com>
2017-10-30 20:33:06 +0600

    project initiated

---
+++ FeedCrawler/FeedCrawler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FeedCrawler.MvcApplication" Language="C#" %>
---
+++ FeedCrawler/FeedCrawler/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FeedCrawler.Startup))]
+namespace FeedCrawler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
